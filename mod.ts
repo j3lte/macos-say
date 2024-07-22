fc92944f969a2190efdb86224335e069938b8780
j3lte <jwlagendijk@gmail.com>
2024-07-22 22:51:09 +0200

    Update codeexamples

--- mod.ts
+++ mod.ts
@@ -10,18 +10,18 @@
  * import { MacOsSay } from '@j3lte/macos-say';
  * // Create a new instance of MacOsSay
  * const sayer = new MacOsSay();
- * const output = await say.say('Hello, World!').exec();
+ * const output = await sayer.say('Hello, World!').exec();
  *
  * // You can also use a static method
  * MacOsSay.say('Hello, World!');
  *
  * // Use options
  * const sayer = new MacOsSay({ voice: 'Alex', rate: 200 });
- * const output = await say.say('Hello, World!').exec();
+ * const output = await sayer.say('Hello, World!').exec();
  *
  * // Chain options
  * const sayer = new MacOsSay().setVoice('Alex').setRate(200);
- * const output = await say.say('Hello, World!').exec();
+ * const output = await sayer.say('Hello, World!').exec();
  * ```
  *
  * ## License
